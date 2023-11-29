7470e2b67b2666d9c922e6a93f3137fa6a815548
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-29 15:29:33 +0200

    challenges

--- check_number.py
+++ check_number.py
@@ -1,2 +1,13 @@
+def check_even_odd(i):
+	return "Even" if i % 2 == 0 else "Odd"
+
+def checker_even_odd(i):
+	if i % 2 == 0:
+		return "Even"
+	else:
+		return "Odd"
+
 number = 7
-print(f"{number} is {check_even_odd(number)}")
+num = 8
+print(f"{number} is {check_even_odd(number)}, but {num} is {check_even_odd(num)}")
+print(f"{number} is {checker_even_odd(number)}, but {num} is {check_even_odd(num)}")
