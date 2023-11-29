207970f41ed35767ade8a3349a0e6a6c28bdd053
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-29 16:48:02 +0200

    challenges

--- last_character.py
+++ last_character.py
@@ -5,13 +5,5 @@ def print_back_string(input):
         i = i - 1
 
 
-def print_back_string(input):
-    i = len(input) - 1
-    while i >= 0:
-        print(input[i])
-        i = i - 1
-
-
 my_string = "welcome"
-print_back_string(my_string)
 print_back_string(my_string)
