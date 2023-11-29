20b0da8dd7ae6ada40afdde285eb90853734d5a8
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-29 15:22:24 +0200

    challenges

--- check_numder.c
+++ check_numder.c
@@ -8,10 +8,17 @@ const char* even_or_odd(int number)
 		return ("Odd");
 }
 
+const char* check_even_odd(int number)
+{
+	return ((number % 2 == 0) ? "Even" : "Odd");
+}
+
 int main() {
 	int number = 7;
 	printf("%d is %s\n", number, even_or_odd(number));
+	printf("%d is %s\n", number, check_even_odd(number));
 	number = 4;
 	printf("%d is %s\n", number, even_or_odd(number));
+	printf("%d is %s\n", number, check_even_odd(number));
 	return (0);
 }
